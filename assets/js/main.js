152871bf14f63e5fe7460b155622f814e412eadb
diyanah08 <diyanah08@gmail.com>
2020-07-08 11:12:41 +0800

    changed footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,4 +162,30 @@ function myFunction() {
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
+ // Get the modal
+ var modal = document.getElementById("contact");
+
+ // Get the button that opens the modal
+ var btn = document.getElementById("contactbtn");
+
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+
+ // When the user clicks on the button, open the modal
+ btn.onclick = function () {
+   modal.style.display = "block";
+ }
+
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function () {
+   modal.style.display = "none";
+ }
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function (event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
+
 })(jQuery);
